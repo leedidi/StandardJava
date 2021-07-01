2ed373db73a9c17246a65f8d543e9d5d4c9390a8
leedidi <edy9527@gmail.com>
2021-07-01 21:04:58 +0900

    7/1 Java

---
+++ src/ch01/Hello.java
@@ -0,0 +1,30 @@
+package ch01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ctrl+shift+L 단축키 전체 목록보기
+		// ctrl+ +,=-=
+		// ctrl+D 한 줄 삭제
+		// ctrl + alt + shift + down 행단위 복사
+		// alt + shift + A 멀티컬럼 편집
+		// alt+up, down 행단위 이동
+		// ctrl+i 자동 들여쓰기
+		// ctrl+/ 주석(토글) -> 한줄 주석
+		// ctrl+space 자동 완성
+		// sysout + ctrl + spcae = system.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("/*주석 아님*/");
+	System.out.println("//Hello,world.*/");
+	System.out.println("Hello,world.");
+	System.out.println("Hello,world.");
+	System.out.println("Hello,world."); 
+	System.out.println();
+	
+	}
+}
