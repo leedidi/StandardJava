b1619722656a585576002e9408972d1a41cd4280
leedidi <edy9527@gmail.com>
2021-07-03 20:26:53 +0900

    7/3 Test2

---
+++ src/ch02/src/ch02/Ex2_1.java
@@ -0,0 +1,16 @@
+package ch02;
+
+public class Ex2_1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout ctrl+Space 자동완성
+		// ctrl+alt+shift+dowm 행단위 복사
+		// alt+shift+a 컬럼 편집 모드(토글)
+		// print() - 출력 후에 줄바꿈을 안함
+		// println() - 출력 후에 줄바꿈을 합니다.
+		System.out.println(3+5); //덧셈
+		System.out.println(5-3); //뻸셈
+		System.out.println(5*3); //곱셈
+		System.out.println(5/3); //나눗셈 +
+	}
+}
