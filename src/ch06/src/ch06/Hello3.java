90d10b53e9b4d5169c9cce7fb0924939f38b53a7
leedidi <edy9527@gmail.com>
2021-07-08 23:37:36 +0900

    7/8 Java

---
+++ src/ch06/src/ch06/Hello3.java
@@ -0,0 +1,10 @@
+package ch06;
+//하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+
+class Hello2 {} //소스파일의 이름은 public class 이름과 일치
+class Hello3 {
+	public static void main(String[] args) {
+		System.out.println("hello"); //class 이름과 제목과 일치해야함 
+	}
+} //하나의 소스파일에는 하나의 public class만 허용
+class Hello4 {} 
